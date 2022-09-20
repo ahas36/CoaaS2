50890478bfe736dd40bb5337705d8a8cbbee75bb
Shakthi Weerasinghe <yasas276@gmail.com>
2022-09-21 00:33:13 +1000

    Same fix for proactive refreshing context

--- CPREE/src/main/java/au/coaas/cpree/executor/scheduler/RefreshScheduler.java
+++ CPREE/src/main/java/au/coaas/cpree/executor/scheduler/RefreshScheduler.java
@@ -5,6 +5,7 @@ import au.coaas.cpree.executor.scheduler.jobs.RegisterClearJob;
 import au.coaas.cpree.utils.PubSub.Event;
 import org.quartz.*;
 
+import java.time.ZoneId;
 import java.util.Date;
 import java.time.LocalDateTime;
 import java.util.logging.Logger;
@@ -55,14 +56,7 @@ public class RefreshScheduler {
                 .build();
 
         LocalDateTime exe_time = LocalDateTime.now().plus(query.getInitInterval(), ChronoUnit.MILLIS);
-        Date triggerTime = new Date(
-                exe_time.getYear(),
-                exe_time.getMonthValue(),
-                exe_time.getDayOfMonth(),
-                exe_time.getHour(),
-                exe_time.getMinute(),
-                exe_time.getSecond()
-                );
+        Date triggerTime = Date.from(exe_time.atZone(ZoneId.systemDefault()).toInstant());
 
         SimpleTrigger trigger = TriggerBuilder.newTrigger()
                 .withIdentity(query.getContextId(), "refreshGroup")

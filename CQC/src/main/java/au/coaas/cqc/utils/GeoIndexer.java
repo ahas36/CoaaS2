f46987cb9f96736b126393bd8b54da316a7ca6d0
Shakthi Weerasinghe <yasas276@gmail.com>
2023-10-10 13:14:46 +1100

    Further functions using version 3

--- CQC/src/main/java/au/coaas/cqc/utils/GeoIndexer.java
+++ CQC/src/main/java/au/coaas/cqc/utils/GeoIndexer.java
@@ -1,11 +1,14 @@
 package au.coaas.cqc.utils;
 
 import au.coaas.cqc.proto.CordinatesIndex;
+
 import com.uber.h3core.H3Core;
-import com.uber.h3core.exceptions.PentagonEncounteredException;
 import com.uber.h3core.util.GeoCoord;
+import com.uber.h3core.exceptions.LineUndefinedException;
+import com.uber.h3core.exceptions.PentagonEncounteredException;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class GeoIndexer {
@@ -56,7 +59,8 @@ public class GeoIndexer {
                 .build();
     }
 
-    // Convert an address or an index to c0ordinates
+    // Convert an address or an index to coordinates.
+    // But this coordinate is the center of the index cell.
     public static CordinatesIndex getCordinates (String address) {
         GeoCoord cordinates = h3Client.h3ToGeo(address);
         return CordinatesIndex.newBuilder().setLat(cordinates.lat)
@@ -71,7 +75,24 @@ public class GeoIndexer {
                 .build();
     }
 
-    // Get k-ring for a given index
+    // Inspection functions.
+    // Get the current resolution of an index. Useful when changing between resolutions.
+    public static int getResolution(long index) {
+        return h3Client.h3GetResolution(index);
+    }
+
+    public static int getResolution(String index) {
+        return h3Client.h3GetResolution(index);
+    }
+
+    // Complete the indexed coordinates object when only address is present.
+    public static CordinatesIndex getIndexForAddress (CordinatesIndex cords) {
+        long index = h3Client.stringToH3(cords.getH3Address());
+        return cords.toBuilder().setIndex(index)
+                .build();
+    }
+
+    // Get all the indices k-levels around a given cell. The output is a hollow circle.
     public static List<Long> getKRing (long index, int k) {
         try {
             return h3Client.hexRing(index, k);
@@ -79,5 +100,41 @@ public class GeoIndexer {
             e.printStackTrace();
         }
         return null;
+    }
+
+    // Get all the indices with in the given distance from a given cell.
+    public static List<Long> getKRange (long index, int k) {
+        try {
+            List<Long> kRange = new ArrayList<>();
+            List<List<Long>> indexesList = h3Client.hexRange(index, k);
+            for(List<Long> indexes : indexesList) {
+                for(long idx : indexes) {
+                    kRange.add(idx);
+                }
+            }
+            return kRange;
+        } catch (PentagonEncounteredException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    // Returns the indexed line between 2 indexes of the same resolution.
+    public static List<Long> getLine (long indexA, long indexB) {
+        try {
+            return h3Client.h3Line(indexA, indexB);
+        } catch (LineUndefinedException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static long getEdge (long index1, long index2) {
+        return h3Client.getH3UnidirectionalEdge(index1, index2);
+    }
+
+    // Returns the higher level index that the provided index belongs to in the hierachy.
+    public static long getParentIndex (long index, int targetRes) {
+        return h3Client.h3ToParent(index, targetRes);
     }
 }
